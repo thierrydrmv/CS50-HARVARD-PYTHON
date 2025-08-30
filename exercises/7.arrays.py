bbf08be32307b6a506dbbf5abed4568ea447c4ff
Thierry de Ridder Martins Varela <varelathierry@gmail.com>
2025-08-29 23:58:04 -0300

    feat: nested loop

--- exercises/7.arrays.py
+++ exercises/7.arrays.py
@@ -6,7 +6,13 @@ def main():
   # exercise4()
   # exercise5()
   # exercise6()
-  exercise7()
+  # exercise7()
+  # exercise8()
+  # exercise9()
+  # exercise10()
+  # exercise11()
+  # exercise12()
+  exercise13()
 
 def exercise1():
   # Write a function `total` that accepts an array of numbers as an argument. The function should return
@@ -112,5 +118,97 @@ def exercise7():
   print(divisors(15)) # [1, 3, 5, 15]
   print(divisors(7)) # [1, 7]
   print(divisors(24)) # [1, 2, 3, 4, 6, 8, 12, 24]
+
+def exercise8():
+  # Write a function `make_acronym` that accepts a sentence string as an argument. The function should
+  # return a string containing the first character of each word in the sentence.
+  def make_acronym(string):
+    string_array = string.split(' ')
+    new_string = ""
+    for i in string_array:
+      new_string += i[0].upper()
+    return new_string
+
+  print(make_acronym("New York")); # NY
+  print(make_acronym("same stuff different day")); # SSDD
+  print(make_acronym("Laugh out loud")); # LOL
+  print(make_acronym("don't over think stuff")); # DOTS
+
+def exercise9():
+  # Write a function `reverse_array` that accepts an array as an argument. The function should return a
+  # array containing the elements of the original array in reverse order.
+  def reverse_array(array):
+    new_array = []
+    for i in range(len(array) - 1, -1, -1):
+      new_array.append(array[i])
+    return new_array
+
+  print(reverse_array(["zero", "one", "two", "three"])); # ['three', 'two', 'one', 'zero']
+  print(reverse_array([7, 1, 8])); # [8, 1, 7]
+
+def exercise10():
+  # Write a function `your_average_function` that accepts an array of numbers as an argument. The
+  # function should return the average of all elements of the array. If the input array is empty,
+  # then the function should return None.
+  def your_average_function(array):
+    if len(array) == 0:
+      return None
+    total = 0
+    for i in array:
+      total += i
+    return total / len(array)
+
+  print(your_average_function([5, 2, 7, 24])); # 9.5
+  print(your_average_function([100, 6])); # 53
+  print(your_average_function([31, 32, 40, 12, 33])); # 29.6
+  print(your_average_function([])); # None
+
+def exercise11():
+  # Write a function `choose_divisibles(numbers, target)` that accepts an array of numbers and a
+  # target number as arguments. The function should return an array containing elements of the original
+  # array that are divisible by the target.
+  def choose_divisibles(numbers, target):
+    new_array = []
+    for i in numbers:
+      if i % target == 0:
+        new_array.append(i)
+    return new_array
+
+  print(choose_divisibles([40, 7, 22, 20, 24], 4)); # [40, 20, 24]
+  print(choose_divisibles([9, 33, 8, 17], 3)); # [9, 33]
+  print(choose_divisibles([4, 25, 1000], 10)); # [1000]
+
+def exercise12():
+  # Write a function `maximum` that accepts an array of numbers as an argument. The function should 
+  # return the largest number of the array. If the array is empty, then the function should return None.
+  def maximum(numbers):
+    if len(numbers) == 0:
+      return None
+    max_num = numbers[0]
+    for i in numbers:
+      if i > max_num:
+        max_num = i
+    return max_num
+
+  print(maximum([5, 6, 3, 7])); # 7
+  print(maximum([17, 15, 19, 11, 2])); # 19
+  print(maximum([])); # None
+
+def exercise13():
+  # Write a function `word_count(sentence, target_words)` that accepts a sentence string and an array of
+  # `target_words`. The function should return a count of the number of words of the sentence that are
+  # in `target_words`.
+  def word_count(sentence, target_words):
+    count = 0
+    sentence_words = sentence.split(" ")
+    for i in range(0, len(sentence_words)):
+      for y in range(0, len(target_words)):
+        if sentence_words[i] == target_words[y]:
+          count += 1
+    return count
+
+  print(word_count("open the window please", ["please", "open", "sorry"])); # 2
+  print(word_count("drive to the cinema", ["the", "driver"])); # 1
+  print(word_count("can I have that can", ["can", "I"])); # 3
 
 main()
